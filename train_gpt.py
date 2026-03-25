22aae42e7e9c4e6da3b97079771f3189d8f5ca2d
mzhong4 <mzhong4@noreply.github.com>
2026-03-25 04:20:55 -0500

    Revert "experiment: rope_base=500"
    This reverts commit f12f640df2d4844c8bdab7fb580207794630206d.

--- train_gpt.py
+++ train_gpt.py
@@ -64,7 +64,7 @@ class Hyperparameters:
     num_heads = int(os.environ.get("NUM_HEADS", 8))
     mlp_mult = float(os.environ.get("MLP_MULT", 2.5))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "1")))
-    rope_base = float(os.environ.get("ROPE_BASE", 500.0))
+    rope_base = float(os.environ.get("ROPE_BASE", 1000.0))
     logit_softcap = float(os.environ.get("LOGIT_SOFTCAP", 30.0))
 
     embed_lr = float(os.environ.get("EMBED_LR", 0.6))
