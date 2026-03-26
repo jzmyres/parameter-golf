8b84d0caa6ccf447d1b116b78e723ce65c38c29c
mzhong4 <mzhong4@noreply.github.com>
2026-03-26 12:24:54 -0500

    Revert "experiment: beta1=0.80"
    This reverts commit 4c187f0d05d996e3552085d46c3065b3db90d52c.

--- train_gpt.py
+++ train_gpt.py
@@ -77,7 +77,7 @@ class Hyperparameters:
     muon_backend_steps = int(os.environ.get("MUON_BACKEND_STEPS", 5))
     muon_momentum_warmup_start = float(os.environ.get("MUON_MOMENTUM_WARMUP_START", 0.92))
     muon_momentum_warmup_steps = int(os.environ.get("MUON_MOMENTUM_WARMUP_STEPS", 1500))
-    beta1 = float(os.environ.get("BETA1", 0.80))
+    beta1 = float(os.environ.get("BETA1", 0.85))
     beta2 = float(os.environ.get("BETA2", 0.95))
     adam_eps = float(os.environ.get("ADAM_EPS", 1e-8))
     grad_clip_norm = float(os.environ.get("GRAD_CLIP_NORM", 0.3))
