3e5bfb1948b2928e95afb0829d66760973d9c33a
mzhong4 <mzhong4@noreply.github.com>
2026-03-26 16:57:10 -0500

    Revert "experiment: swa_every=20"
    This reverts commit 45558f3f5fb280cf446664ab161cfc0484e423c6.

--- train_gpt.py
+++ train_gpt.py
@@ -92,7 +92,7 @@ class Hyperparameters:
 
     swa_enabled = bool(int(os.environ.get("SWA_ENABLED", "1")))
     swa_start_frac = float(os.environ.get("SWA_START_FRAC", 0.3))
-    swa_every = int(os.environ.get("SWA_EVERY", 20))
+    swa_every = int(os.environ.get("SWA_EVERY", 25))
 
 # -----------------------------
 # MUON OPTIMIZER
