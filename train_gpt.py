d9412922e8150269badd8c4e1d42a310b4a9c9cf
mzhong4 <mzhong4@noreply.github.com>
2026-03-27 12:00:11 -0500

    Revert "experiment: muon_warmup_start=0.85 (match beta1)"
    This reverts commit d4439d27d0790c6a8c5d8251ee2f7544ad2af8e0.

--- train_gpt.py
+++ train_gpt.py
@@ -75,7 +75,7 @@ class Hyperparameters:
     scalar_lr = float(os.environ.get("SCALAR_LR", 0.03))
     muon_momentum = float(os.environ.get("MUON_MOMENTUM", 0.99))
     muon_backend_steps = int(os.environ.get("MUON_BACKEND_STEPS", 5))
-    muon_momentum_warmup_start = float(os.environ.get("MUON_MOMENTUM_WARMUP_START", 0.85))
+    muon_momentum_warmup_start = float(os.environ.get("MUON_MOMENTUM_WARMUP_START", 0.92))
     muon_momentum_warmup_steps = int(os.environ.get("MUON_MOMENTUM_WARMUP_STEPS", 1500))
     beta1 = float(os.environ.get("BETA1", 0.85))
     beta2 = float(os.environ.get("BETA2", 0.90))
